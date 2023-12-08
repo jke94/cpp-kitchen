6a0be8d01eda439558109b124ec1174efd234335
jke94 <jcarracedo.dev@gmail.com>
2023-12-08 20:31:37 +0100

    Update with complex example.

--- 10.goatnative-inject-investigation/main.cpp
+++ 10.goatnative-inject-investigation/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <typeinfo>
 
 #include "Injector.h"
 
@@ -179,12 +180,108 @@ void testFactory()
     assert(injector.getInstance<Notifier>().get() != injector.getInstance<Notifier>().get());
 }
 
+class IDummy
+{
+    public:
+        virtual int get_number() = 0;
+};
+
+class Dummy : public IDummy
+{
+    public:
+        int get_number() override
+        {
+            return 1994;
+        }
+};
+
+class ISuperDummy
+{
+    public:
+        virtual int get_number() = 0;
+};
+
+class SuperDummy : public ISuperDummy
+{
+    private:
+        shared_ptr<IDummy> dummy_;
+
+    public:
+        SuperDummy(shared_ptr<IDummy> dummy) : dummy_ (dummy){}
+        SuperDummy();
+
+        int get_number() override
+        {
+            return 1994;
+        }
+};
+
+class IMegaDummy
+{
+    public:
+        virtual int get_number() = 0;
+};
+
+class MegaDummy : public IMegaDummy
+{
+    private:
+        shared_ptr<ISuperDummy> super_dummy_;
+
+    public:
+        MegaDummy(shared_ptr<ISuperDummy> super_dummy) : super_dummy_ (super_dummy){}
+        MegaDummy();
+
+        int get_number() override
+        {
+            return 1994;
+        }
+};
+
+void test_with_several_dependencies()
+{
+    Injector injector;
+
+    injector.registerClass<Dummy>();
+    injector.registerInterface<IDummy,Dummy>();
+
+    injector.registerClass<SuperDummy,IDummy>();
+    injector.registerInterface<ISuperDummy,SuperDummy>();
+
+    injector.registerClass<MegaDummy,ISuperDummy>();
+    injector.registerInterface<IMegaDummy,MegaDummy>();
+
+    auto dummy = injector.getInstance<IDummy>();
+    auto super_dummy = injector.getInstance<ISuperDummy>();
+    auto mega_dummy = injector.getInstance<IMegaDummy>();
+
+    cout << "Memory: " << dummy.get() << ", value: " << dummy.get()->get_number() << endl;
+    cout << "Memory: " << super_dummy.get() << ", value: " << super_dummy->get_number() << endl;
+    cout << "Memory: " << mega_dummy.get() << ", value: " << mega_dummy.get()->get_number() << endl;
+
+    std::vector<shared_ptr<IMegaDummy>> mega_dummy_vector;
+
+    mega_dummy_vector.push_back(injector.getInstance<IMegaDummy>());
+    mega_dummy_vector.push_back(injector.getInstance<IMegaDummy>());
+    mega_dummy_vector.push_back(injector.getInstance<IMegaDummy>());
+    mega_dummy_vector.push_back(injector.getInstance<IMegaDummy>());
+
+    std::cout << "-----------------------------------------" <<std::endl;
+
+    for(auto item : mega_dummy_vector)
+    {
+        std::cout << "Memory: " << item << std::endl;
+    }
+
+}
+
 int main(int argc, const char * argv[]) {
     // TODO - move tests to Google Test
     testSingleton();
     testBuildWholeGraph();
     testFactory();
 	testInterfaceBasedFactory();
+
+    test_with_several_dependencies();
 
     return 0;
 }
