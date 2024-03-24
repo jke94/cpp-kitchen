82f16b995430954c55a5ed53b7bb9965b058a7ce
jke94 <jcarracedo.dev@gmail.com>
2024-03-24 12:34:49 +0100

    Implementation a life of the person based on thread.

---
+++ 18.cpp-thereads-with-shared-memory/IPerson.h
@@ -0,0 +1,10 @@
+#include <string>
+
+class IPerson
+{
+    public:
+        virtual ~IPerson() {};
+        virtual std::string get_name() = 0;
+        virtual int get_ppm() = 0;
+        virtual void set_ppm(int ppm) = 0;
+};
